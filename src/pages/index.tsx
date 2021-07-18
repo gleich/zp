48e4c5ab1a9f7f29e8454203e23d3a45e89f0ca6
Matt Gleich <git@mattglei.ch>
2021-07-18 17:10:04 -0400

    chore(structure): move to src and tsconfig paths
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import { Meta } from './components/meta'
+import styles from '@styles/Home.module.css'
+import { Meta } from '@components/meta'
 
 export default function Home() {
   return (
