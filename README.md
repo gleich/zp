999e4c2f8ec42b0b0e3666d220900f238d1bd371
Matt Gleich <git@mattglei.ch>
2021-07-19 16:36:28 -0500

    feat: rename project to zephyr people
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# zpl; The [Zephyr](https://zephyr.hackclub.com/) People Log
+# zp; The [Zephyr](https://zephyr.hackclub.com/) People Log
 
 🚂 Website for all the cool [Zephyr](https://zephyr.hackclub.com/) guests; Made in a day on a train across the country with 42 other hackclubbers.
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import Button from '@components/Button'
 export default function Home() {
   return (
     <div>
-      <Meta title="zpl" />
+      <Meta title="zp" />
       <body className={styles.body}>
         <div className={styles.title}>
           <h1 className={styles.titleWord}>Zephyr</h1>
