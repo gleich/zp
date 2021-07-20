f7d6a0ac9a54bd5f61f25843950a069628483aad
Matt Gleich <git@mattglei.ch>
2021-07-20 13:08:57 -0400

    feat: finish main meet em' page
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -1,1 +1,20 @@
-export default function Person() {}
+/* eslint-disable @next/next/no-img-element */
+import styles from '@styles/components/Person.module.css'
+
+export default function Person({
+  name,
+  description,
+}: {
+  name: string
+  description: string
+}) {
+  return (
+    <main className={styles.main}>
+      <img src="test.jpg" alt={name} />
+      <div className={styles.info}>
+        <h2>{name}</h2>
+        <h4>{description}</h4>
+      </div>
+    </main>
+  )
+}
--- src/pages/meet.tsx
+++ src/pages/meet.tsx
@@ -1,16 +1,18 @@
 import styles from '@styles/pages/Meet.module.css'
 import Meta from '@components/Meta'
+import Person from '@components/Person'
 
 export default function Meet() {
   return (
     <div>
       <Meta title="Meet em'" />
       <body className={styles.body}>
-        <h1>Meet em&apos;</h1>
-        <p>
+        <h1 className={styles.title}>Meet em&apos;</h1>
+        <h3>
           Meet all the cool people we&apos;ve seen along the{' '}
           <a href="https://zephyr.hackclub.com">Zephyr hackclub trip.</a>
-        </p>
+        </h3>
+        <Person name="Mr. Test" description="A very cool person" />
       </body>
     </div>
   )
